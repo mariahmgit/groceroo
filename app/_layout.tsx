8ff1983b9bc66825bc3a1a55fa3af2ecbe1c23df
mariahmgit <mariahmensah01@gmail.com>
2026-04-23 22:23:46 -0500

    added react navigation and nativewind. setup root navigation router

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+// app/_layout.tsx
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="details" options={{ title: 'Details' }} />
+    </Stack>
+  );
+}
